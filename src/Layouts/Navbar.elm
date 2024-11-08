484b2702907c69babe17a6355e51cd57dcb9ad6c
Max Burri <burri@puzzle.ch>
2024-11-08 13:35:22 +0100

    feat(ui): add title to all pages

--- src/Layouts/Navbar.elm
+++ src/Layouts/Navbar.elm
@@ -3,8 +3,10 @@ module Layouts.Navbar exposing (Model, Msg, Props, layout)
 import Effect exposing (Effect)
 import Element exposing (..)
 import Element.Background as Background
+import Element.Font as Font
 import Layout exposing (Layout)
 import Route exposing (Route)
+import Route.Path exposing (Path)
 import Shared
 import View exposing (View)
 
@@ -18,7 +20,7 @@ layout props shared route =
     Layout.new
         { init = init
         , update = update
-        , view = view
+        , view = view route
         , subscriptions = subscriptions
         }
 
@@ -64,20 +66,21 @@ subscriptions model =
 -- VIEW
 
 
-view : { toContentMsg : Msg -> contentMsg, content : View contentMsg, model : Model } -> View contentMsg
-view { toContentMsg, model, content } =
+view : Route () -> { toContentMsg : Msg -> contentMsg, content : View contentMsg, model : Model } -> View contentMsg
+view route { toContentMsg, model, content } =
     { title = content.title
     , attributes = []
     , element =
         column [ width fill, height fill ]
             [ row
                 [ width fill
-                , Background.color (rgb255 222 222 222)
+                , Background.color (rgb255 0 0 0)
                 ]
                 [ row
                     [ spacing 16
                     , padding 16
                     , alignRight
+                    , Font.color (rgba255 255 255 255 0.55)
                     ]
                     [ el [] <| link [] { url = "/apps", label = text "Apps" }
                     , el [] <| link [] { url = "/servers", label = text "Servers" }
@@ -86,6 +89,37 @@ view { toContentMsg, model, content } =
                     , el [] <| link [] { url = "/settings", label = text "Settings" }
                     ]
                 ]
+            , row
+                [ width fill
+                , paddingXY 16 12
+                , Background.color (rgb255 222 222 222)
+                ]
+                [ el [ Font.bold, Font.size 18 ] <| text (toPageTitle route.path) ]
             , el [ padding 16, width fill, height fill ] <| content.element
             ]
     }
+
+
+toPageTitle : Path -> String
+toPageTitle path =
+    case path of
+        Route.Path.Home_ ->
+            "Home"
+
+        Route.Path.Apps ->
+            "Apps"
+
+        Route.Path.Deploy ->
+            "Deploy"
+
+        Route.Path.Resources ->
+            "Resources"
+
+        Route.Path.Servers ->
+            "Servers"
+
+        Route.Path.Settings ->
+            "Settings"
+
+        Route.Path.NotFound_ ->
+            "Not Found"
